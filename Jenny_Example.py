2d0fb14f139f2e1e64bcc98ba691a50009f1e7d4
Jenny940201 <ljenny9421@gmail.com>
2026-02-25 11:56:55 +0800

    Add files via upload

---
+++ Jenny_Example.py
@@ -0,0 +1,13 @@
+
+purchase_amount = 350
+discount_a = purchase_amount*0.1
+discount_b = purchase_amount*0.05
+discount_c = purchase_amount
+
+if purchase_amount>=500:
+    print("Discount=",discount_a)
+elif purchase_amount>=200 and purchase_amount<500:
+    print("Discount=",discount_b)
+else:
+    purchase_amount<200
+    print("Discount=",discount_c)
